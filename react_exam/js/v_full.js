83bb6771647e77a2e88ad16cafa92c151c14194a
sylvialee <142771992+sylvialee1@users.noreply.github.com>
2023-11-08 16:45:55 +0900

    Add files via upload

---
+++ react_exam/js/v_full.js
@@ -0,0 +1,14 @@
+const cover Image =document.querySelector("#cover");
+
+coverImage.onclick = function(){
+alert("눌렀습니다.");
+
+coverImage.onmouseover = function(){
+
+coverImage.stlye.border = "5px solid black";
+};
+
+coverImage.onmouseout = function() {
+coverImage.stlye.border = "";
+
+}
